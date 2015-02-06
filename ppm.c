b0a21c9b5c611cd4a89d06bc2157c63af00d9f0b
Staroselskii Georgii <gosha371@gmail.com>
2015-02-06 16:30:18 +0300

    Reimplemented using raw memory functions

--- ppm.c
+++ ppm.c
@@ -2,16 +2,9 @@
 #include <linux/kernel.h>
 #include <linux/gpio.h>
 #include <linux/interrupt.h> 
+#include <asm/io.h>
 
-/* Define GPIOs for LEDs */
-static struct gpio leds[] = {
-		{  24, GPIOF_OUT_INIT_LOW, "PIN 24" },
-};
-
-/* Define GPIOs for BUTTONS */
-static struct gpio buttons[] = {
-		{ 4, GPIOF_IN, "PPM 1" },	// turns LED on
-};
+#include "rpi.h"
 
 /* Later on, the assigned IRQ numbers for the buttons are stored here */
 static int button_irqs[] = { -1, -1 };
@@ -22,8 +15,13 @@ static int button_irqs[] = { -1, -1 };
 static irqreturn_t button_isr(int irq, void *data)
 {
 	if(irq == button_irqs[0]) {
-        int value = gpio_get_value(leds[0].gpio);
-		gpio_set_value(leds[0].gpio, value == 1? 0: 1);
+        int value = GPIO_READ(24) ? 1: 0;
+		if (value == 1) {
+            GPIO_SET_LOW = 1 << 24;
+        } else {
+            GPIO_SET_HIGH = 1 << 24;
+        }
+
 	}
 
 	return IRQ_HANDLED;
@@ -38,8 +36,18 @@ static int __init ppm_init(void)
 
 	printk(KERN_INFO "%s\n", __func__);
 
+    gpio.map     = ioremap(GPIO_BASE, 4096);//p->map;
+	gpio.addr    = (volatile unsigned int *)gpio.map;
+
+    printk(KERN_INFO "%p %p\n", gpio.map, gpio.addr);
+
+    INP_GPIO(4);	
+
+	INP_GPIO(24);
+	OUT_GPIO(24);
+        
 	// register LED gpios
-	ret = gpio_request_array(leds, ARRAY_SIZE(leds));
+	ret = gpio_request(24, "PIN 24" );
 
 	if (ret) {
 		printk(KERN_ERR "Unable to request GPIOs for LEDs: %d\n", ret);
@@ -47,16 +55,16 @@ static int __init ppm_init(void)
 	}
 	
 	// register BUTTON gpios
-	ret = gpio_request_array(buttons, ARRAY_SIZE(buttons));
+	ret = gpio_request(4, "PPM PIN");
 
 	if (ret) {
-		printk(KERN_ERR "Unable to request GPIOs for BUTTONs: %d\n", ret);
+		printk(KERN_ERR "Unable to request GPIOs for PPM: %d\n", ret);
 		goto fail1;
 	}
 
-	printk(KERN_INFO "Current button1 value: %d\n", gpio_get_value(buttons[0].gpio));
+	printk(KERN_INFO "Current PPM value: %d\n", GPIO_READ(4) ? 1: 0);
 	
-	ret = gpio_to_irq(buttons[0].gpio);
+	ret = gpio_to_irq(4);
 
 	if(ret < 0) {
 		printk(KERN_ERR "Unable to request IRQ: %d\n", ret);
@@ -65,9 +73,9 @@ static int __init ppm_init(void)
 
 	button_irqs[0] = ret;
 
-	printk(KERN_INFO "Successfully requested BUTTON1 IRQ # %d\n", button_irqs[0]);
+	printk(KERN_INFO "Successfully requested PPM PIN4 IRQ # %d\n", button_irqs[0]);
 
-	ret = request_irq(button_irqs[0], button_isr, IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING, "gpiomod#button1", NULL);
+	ret = request_irq(button_irqs[0], button_isr, IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING, "ppm#pin4", NULL);
 
 	if(ret) {
 		printk(KERN_ERR "Unable to request IRQ: %d\n", ret);
@@ -80,10 +88,10 @@ static int __init ppm_init(void)
 // cleanup what has been setup so far
 
 fail2: 
-	gpio_free_array(buttons, ARRAY_SIZE(leds));
+	gpio_free(24);
 
 fail1:
-	gpio_free_array(leds, ARRAY_SIZE(leds));
+	gpio_free(4);
 
 	return ret;	
 }
@@ -97,17 +105,21 @@ static void __exit ppm_exit(void)
 
 	printk(KERN_INFO "%s\n", __func__);
 
-	// free irqs
 	free_irq(button_irqs[0], NULL);
 	
-	// turn all LEDs off
-	for(i = 0; i < ARRAY_SIZE(leds); i++) {
-		gpio_set_value(leds[i].gpio, 0); 
-	}
+
+    GPIO_SET_LOW = 1 << 24;
+	//for(i = 0; i < ARRAY_SIZE(leds); i++) {
+	//	gpio_set_value(leds[i].gpio, 0); 
+	//}
 	
-	// unregister
-	gpio_free_array(leds, ARRAY_SIZE(leds));
-	gpio_free_array(buttons, ARRAY_SIZE(buttons));
+	gpio_free(24);
+	gpio_free(4);
+
+    if (gpio.addr){
+        /* release the mapping */
+        iounmap(gpio.addr);
+	}
 }
 
 MODULE_LICENSE("GPL");
---
+++ rpi.h
@@ -0,0 +1,23 @@
+#define BCM2708_PERI_BASE       0x20000000
+#define GPIO_BASE               (BCM2708_PERI_BASE + 0x200000)	// GPIO controller 
+ 
+ 
+#define INP_GPIO(g)   *(gpio.addr + ((g)/10)) &= ~(7<<(((g)%10)*3))
+#define OUT_GPIO(g)   *(gpio.addr + ((g)/10)) |=  (1<<(((g)%10)*3))
+ 
+#define GPIO_SET_HIGH       *(gpio.addr + 7)  // sets   bits which are 1
+#define GPIO_SET_LOW        *(gpio.addr + 10) // clears bits which are 1
+
+#define GPIO_CLR  *(gpio.addr + 10) 
+ 
+#define GPIO_READ(g)  *(gpio.addr + 13) & (1<<(g))
+
+// IO Acces
+struct bcm2835_peripheral {
+    unsigned long addr_p;
+    int mem_fd;
+    void *map;
+    volatile unsigned int *addr;
+};
+
+struct bcm2835_peripheral gpio = {GPIO_BASE};
